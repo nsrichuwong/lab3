1270d0006e1f92b577e033d068a2d29438fbcea1
lab302-49 <lab302-49@labeng.local>
2019-01-11 17:50:18 +0700

    change number

---
+++ lab_3_3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    double x = 2, sum = 0;
+    while (x < 69){
+        sum += 1/x;
+        x = x+1;
+    }
+    cout << "sum is " << sum;
+    
+    return 0;
+}
